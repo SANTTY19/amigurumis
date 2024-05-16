c04bfdd5cffa9b22c40a5422837b1b4dd6095535
SANTTY19 <santiago123araya@gmail.com>
2024-05-16 01:22:03 -0600

    cambio principal

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
-// Función para alternar entre los artículos del aside
-function alternarProductos() {
-    const productos = document.querySelectorAll('.producto'); // Selecciona todos los artículos
-    let indice = 0; // Índice del artículo actual
+// Función para alternar entre los artículos de productos destacados
+function alternarProductosDestacados() {
+    const productos = document.querySelectorAll('.productos .producto'); // Selecciona todos los productos
+    let indice = 0; // Índice del producto actual
 
-    // Función para mostrar el siguiente artículo
+    // Función para mostrar el siguiente producto
     function mostrarSiguienteProducto() {
         // Oculta el producto actual
         productos[indice].classList.remove('visible');
@@ -11,18 +11,18 @@ function alternarProductos() {
         // Incrementa el índice y vuelve al primero si llega al final
         indice = (indice + 1) % productos.length;
 
-        // Muestra el siguiente artículo
+        // Muestra el siguiente producto
         productos[indice].classList.add('visible');
     }
 
-    // Muestra el primer artículo
+    // Muestra el primer producto
     productos[indice].classList.add('visible');
 
-    // Intervalo para cambiar automáticamente los artículos cada 5 segundos
+    // Intervalo para cambiar automáticamente los productos cada 5 segundos
     setInterval(mostrarSiguienteProducto, 2000);
 }
 
 // Llama a la función al cargar la página
 window.onload = function() {
-    alternarProductos();
+    alternarProductosDestacados();
 };
